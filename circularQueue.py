4e4fe000033d15242d294c06427dcf40e5e8ff40
Thomas Eley <thomas-eley@hotmail.co.uk>
2020-10-06 10:59:08 +0400

    Circular Queue

--- circularQueue.py
+++ circularQueue.py
@@ -1,1 +1,47 @@
 #Solution to Circular Queue Problem Set
+class cirQueue:
+    pass
+#Initialization: Size, Queue, Head Pointer, Tail Pointer
+
+#Enqueue:
+#if self.isEmpty():
+#tailP = 0
+#headP = 0
+#queue[tailP] = item
+#elif not self.isfull():
+#tailp = (tailp + 1)%Size
+#queue[tailP] = item
+#else:
+#print("Full")
+
+#Dequeue:
+#if not isEmpty:
+#item = queue[headP]
+#if tailp == headp:
+#reset Queue
+#else:
+#head = (head+1)%size
+#else:
+#print("Empty")
+
+#Peek:
+#if self.isEmpty():
+#print("Empty")
+#else:
+#print(self.__queue[self.__headP])
+
+#isFull:
+#if (tailP+1)%size = headP
+#return True
+#else:
+#return False
+
+#isEmpty:
+#if tailP == -1 and headP == -1:
+#return True
+#else:
+#return False
+
+#TESTING
+
+#ERROR HANDLING TESTING
